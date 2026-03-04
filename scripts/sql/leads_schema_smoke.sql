026f87d5248c25d32b4c3d2891d94af6a9078875
Ilan Chelly <ilanchelly@gmail.com>
2026-03-04 16:23:50 +0200

    feat: add leads import schema rls and rpc

---
+++ scripts/sql/leads_schema_smoke.sql
@@ -0,0 +1,3 @@
+select to_regclass('public.lead_import_batches') as batches_table;
+select to_regclass('public.leads') as leads_table;
+select to_regclass('public.lead_import_rejections') as rejections_table;
